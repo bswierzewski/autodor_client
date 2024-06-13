3e06d90cb496a4a9a5eb7aca18f23426da143426
bswierzewski <swierzewski.bartosz@gmail.com>
2024-06-13 22:33:36 +0200

    Add sesion expires sync with jwt token

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import Auth0Provider from 'next-auth/providers/auth0';
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import NextAuth, { NextAuthOptions, Session } from 'next-auth';
 
 const authOptions: NextAuthOptions = {
   session: {
@@ -27,15 +27,20 @@ const authOptions: NextAuthOptions = {
     async jwt({ token, account }) {
       if (account) {
         token.access_token = account.access_token;
-      }
+        token.expires_at = account.expires_at;
+      }      
 
       return token;
     },
     async session({ session, token }) {
       if (token) {
         session.access_token = token.access_token;
+        // expires_at is in seconds, convert to milliseconds
+        if (token.expires_at && Date.now() > (token.expires_at * 1000)) {
+          return {} as Session;
+        }
       }
-
+    
       return session;
     },
     async redirect({ url, baseUrl }) {
