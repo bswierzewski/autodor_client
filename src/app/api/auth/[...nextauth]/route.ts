6c499531306325b2d1f83257b7c3e57e39f79bd9
bswierzewski <swierzewski.bartosz@gmail.com>
2024-06-17 09:57:26 +0200

    Install skeleton

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import Auth0Provider from 'next-auth/providers/auth0';
 import NextAuth, { NextAuthOptions, Session } from 'next-auth';
+import Auth0Provider from 'next-auth/providers/auth0';
 
 const authOptions: NextAuthOptions = {
   session: {
@@ -7,8 +7,8 @@ const authOptions: NextAuthOptions = {
   },
   providers: [
     Auth0Provider({
-      clientId: <string>process.env.AUTH0_CLIENTID,
-      clientSecret: <string>process.env.AUTH0_CLIENTSECRET,
+      clientId: process.env.AUTH0_CLIENTID as string,
+      clientSecret: process.env.AUTH0_CLIENTSECRET as string,
       issuer: process.env.AUTH0_ISSUER,
       idToken: true,
       token: {
@@ -18,7 +18,7 @@ const authOptions: NextAuthOptions = {
       },
       authorization: {
         params: {
-          audience: encodeURI(<string>process.env.AUTH0_AUDIENCE)
+          audience: encodeURI(process.env.AUTH0_AUDIENCE as string)
         }
       }
     })
@@ -28,7 +28,7 @@ const authOptions: NextAuthOptions = {
       if (account) {
         token.access_token = account.access_token;
         token.expires_at = account.expires_at;
-      }      
+      }
 
       return token;
     },
@@ -36,11 +36,11 @@ const authOptions: NextAuthOptions = {
       if (token) {
         session.access_token = token.access_token;
         // expires_at is in seconds, convert to milliseconds
-        if (token.expires_at && Date.now() > (token.expires_at * 1000)) {
+        if (token.expires_at && Date.now() > token.expires_at * 1000) {
           return {} as Session;
         }
       }
-    
+
       return session;
     },
     async redirect({ url, baseUrl }) {
