f60cb56f764cf92653c894ac1282f9df0824b7dd
bswierzewski <swierzewski.bartosz@gmail.com>
2024-06-20 13:58:23 +0200

    Change logo and favicon

--- src/components/nav/components/Logo.tsx
+++ src/components/nav/components/Logo.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
-import React, { useEffect, useMemo, useState } from 'react';
+
+import logo from '../../../../public/blue_horizontal.png';
 
 type Props = {
   size: number;
@@ -13,28 +13,17 @@ type Props = {
 export default function Logo({ className, size }: Props) {
   const router = useRouter();
   const pathname = usePathname();
-  const { theme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
   const doReset = () => {
     if (pathname !== '/') router.push('/');
   };
-
-  const logoSrc = useMemo(() => {
-    if (!mounted) return '/logo-light.svg'; // Default or placeholder logo
-    return theme === 'dark' ? '/logo-dark.svg' : '/logo-light.svg';
-  }, [theme, mounted]);
 
   return (
     <div
       onClick={doReset}
       className={`${className} cursor-pointer flex items-center gap-2 text-3xl font-semibold mx-5`}
     >
-      <Image src={logoSrc} height={size} width={size} alt={mounted ? `${theme} theme logo` : 'logo'} />
+      <Image src={logo} height={size} width={size} alt="logo" />
     </div>
   );
 }
